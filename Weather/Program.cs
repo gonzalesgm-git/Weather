49ac0a6767da5e504e0442c3b36c494b66b8bea3
VISUALRISK\ggonzales <ggonzales@gtreasury.com>
2024-03-27 16:33:50 +0800

    ignore null values from results

--- Weather/Program.cs
+++ Weather/Program.cs
@@ -5,6 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddHttpClient<RainfallService>();
 builder.Services.AddControllers();
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.IgnoreNullValues = true;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
